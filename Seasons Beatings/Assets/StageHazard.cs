3c235250ff344790755bc6bbdb8e8c30af26ef29
LilGenius03 <finnarcher03@gmail.com>
2024-01-23 21:48:21 +0000

    stage hazard added

---
+++ Seasons Beatings/Assets/StageHazard.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageHazard : MonoBehaviour
+{
+    HealthSystem healthSystem;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthSystem= Player.GetComponent<HealthSystem>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            healthSystem.HammerDamage++;
+        }
+    }
+}
