240aabd7175ac6dd3d8a938b86238116ce2840b2
LilGenius03 <finnarcher03@gmail.com>
2024-01-23 21:35:08 +0000

    updated health script

--- Seasons Beatings/Assets/Scripts/HealthSystem.cs
+++ Seasons Beatings/Assets/Scripts/HealthSystem.cs
@@ -8,13 +8,15 @@ public class HealthSystem : MonoBehaviour
 {
     public float Playerhealth = 10f;
     public float HammerDamage = 1f;
+    public float KnockBackForce = 1f;
     private bool IsDead = false;
     public GameObject BloodEffect;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -30,6 +32,10 @@ public class HealthSystem : MonoBehaviour
             Playerhealth -= HammerDamage;
             Instantiate(BloodEffect, transform.position, Quaternion.identity);
             Destroy(BloodEffect, 1f);
+            Vector2 difference = (gameObject.transform.position - transform.position).normalized;
+            Vector2 force = difference * KnockBackForce;
+            rb.AddForce(force, ForceMode2D.Impulse);
+        
 
             if (Playerhealth <= 0 && IsDead == true)
             {
